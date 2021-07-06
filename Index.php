052ba8d787d33a214105bc2fceeb6bc599c0458d
Ortiz <alfonso.ortiz@audatex.com>
2021-07-06 12:15:44 -0500

    Version 1

---
+++ Index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Prueba de PHP para Jenkins Index</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo Index</p>'; ?>
+ <h1>Hola Mundo Jenkins Index</h1>
+ </body>
+</html>
