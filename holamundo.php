9f8edf0e78214e5728dbc2cb7f239a687e8c0b3a
Ortiz <alfonso.ortiz@audatex.com>
2021-07-06 11:55:31 -0500

    version 1

---
+++ holamundo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Prueba de PHP para Jenkins</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ <h1>Hola Mundo Jenkins </h1>
+ </body>
+</html>
